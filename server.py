c53c41608c1f3c168715e4c3f84ab70dc6e47e84
unknown <patrikmagalhaes@icloud.com>
2024-09-06 13:39:12 -0300

    pegando menor quaidade de audio

--- server.py
+++ server.py
@@ -16,11 +16,17 @@ def download_audio():
     try:
         # Baixar o áudio do YouTube
         yt = YouTube(url, on_progress_callback=on_progress)
-        yt_title = yt.title
+  
 
-        # Obter o áudio apenas e salvar como MP3 com um nome específico
-        ys = yt.streams.get_audio_only()
-        audio_file = ys.download(filename="audio.mp3")  # Salva como audio.mp3
+        # Obter a stream de áudio de menor qualidade
+        audio_streams = yt.streams.filter(only_audio=True).order_by('bitrate')  # Ordena pela taxa de bits
+        lowest_quality_stream = audio_streams.first()  # Pega a stream de menor qualidade
+
+        if not lowest_quality_stream:
+            return jsonify({'error': 'No audio streams available'}), 404
+
+        # Salvar como MP3 com um nome específico
+        audio_file = lowest_quality_stream.download(filename="audio.mp3")  # Salva como audio.mp3
 
         return send_file(audio_file, as_attachment=True, download_name="audio.mp3")
 
